7a6549d9ee066bd46541b2bf10b459bb58fa67fa
yasuaki0714 <74097636+yasuaki0714@users.noreply.github.com>
2020-12-18 08:07:38 +0900

    追加

--- 02_コーディング練習/01_模写コーディング/03_kyakukuru/root/js/script.js
+++ 02_コーディング練習/01_模写コーディング/03_kyakukuru/root/js/script.js
@@ -1,3 +1,22 @@
+//　ハンバーガーメニュー
+$('.spMenuToggle').on('click',function() {
+  $('.spMenuToggle, .spNav, .open, .close').toggleClass('show');
+});
+
+//　ページ内検索　モーダル
+$('.surchBtn').on('click',function() {
+  $('.surchMordal').toggleClass('show');
+});
+
+$(function(){
+  $('.surchMordal').click(function(){
+    $('.surchMordal').toggleClass('show');
+  });
+  $('#searchform').on('click', function(e){
+    e.stopPropagation();
+  })
+});
+
 // スムーズスクロール
 $('a[href^="#"]').click(function(){
   var speed = 500;
@@ -8,7 +27,7 @@ $('a[href^="#"]').click(function(){
   return false;
 });
 
-// スクロールするとページトップボタンが現れる
+// スクロールするとページトップボタンが現れる PC
 $(function(){
   var topBtn = $('#pageTopBtn');
   topBtn.hide();
@@ -21,18 +40,18 @@ $(function(){
   })
 });
 
-
+// カルーセル
 $('.slick01').slick({
   dots: true,
   arrows: true,
   prevArrow:'<div class="prev">＜</div>',
 	nextArrow:'<div class="next">＞</div>',
-  // autoplay: true,
-  // autoplaySpeed: 500,
+  autoplay: true,
+  autoplaySpeed: 5000,
   centerMode: true,
   centerPadding: '5%',
   responsive: [{
-    breakpoint: 768,
+    breakpoint: 767,
     settings: {
       arrows: false,
       centerMode: false,
@@ -47,6 +66,10 @@ $('.slick02').slick({
   centerPadding: '50px',
   slidesToShow: 3,
   slidesToScroll: 3,
+  variableWidth: true,
+  arrows: true,
+  prevArrow:'<div class="prev">＜</div>',
+	nextArrow:'<div class="next">＞</div>',
 });
 
 $('.slick03').slick({
---
+++ 02_コーディング練習/02_XD/02_クリ★スタ/02_中級/root/js/script.js
@@ -0,0 +1,48 @@
+$('.slick01').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  variableWidth: true,
+});
+
+
+$(function () {
+  const ua = navigator.userAgent;
+  if (ua.indexOf('iPhone') > -1 || (ua.indexOf('Android') > -1 && ua.indexOf('Mobile') > -1)) {
+    $(function(){
+      $(window).scroll(function(){
+        if($(this).scrollTop() > 291){
+          $('.header').addClass('fixed');
+        } else {
+          $('.header').removeClass('fixed');
+        }
+      });
+    });
+  } else {
+    $(function(){
+      $(window).scroll(function(){
+        if($(this).scrollTop() > 652){
+          $('.header').addClass('fixed');
+        } else {
+          $('.header').removeClass('fixed');
+        }
+      });
+    });
+  }
+});
+
+// スムーズスクロール
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+
+//ハンバーガーメニュー
+$('.headerNavToggle').on('click',function(){
+  $('.headerNavToggle, headerNav').toggleClass('show');
+});
